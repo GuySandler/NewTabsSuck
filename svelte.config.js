c2648a6b9681a3ece99cced4bef12b464dea52bf
Guy Sandler <guy@sandler.co.il>
2025-01-31 22:05:12 -0800

    vercel update

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,21 @@
-import adapter from "sveltekit-adapter-chrome-extension";
+// import adapter from "sveltekit-adapter-chrome-extension";
+
+// export default {
+//   kit: {
+//     adapter: adapter({
+//       pages: "build",
+//       assets: "build",
+//       fallback: null,
+//       precompress: false,
+//       manifest: "manifest.json",
+//     }),
+//     appDir: "app",
+//   },
+// };
+import adapter from "@sveltejs/adapter-vercel";
 
 export default {
-  kit: {
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: null,
-      precompress: false,
-      manifest: "manifest.json",
-    }),
-    appDir: "app",
-  },
+	kit: {
+		adapter: adapter(),
+	},
 };
